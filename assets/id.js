9464027ae3e5b7a66a7aec733a414d9d29dd566c
SzybkiInwalida <szybki.inwalida@gmail.com>
2026-06-15 22:12:19 +0200

    Update id.js

--- assets/id.js
+++ assets/id.js
@@ -17,7 +17,6 @@ const auth = getAuth(app);
 const db = getDatabase(app);
 
 var params = new URLSearchParams(window.location.search);
-
 var date = new Date();
 document.querySelector(".welcome").innerHTML = date.getHours() >= 18 ? "Dobry wieczór!" : "Dzień dobry!";
 
@@ -58,46 +57,3 @@ async function login() {
     alert("Błędne hasło!");
   }
 }
-
-input.addEventListener("input", () => {
-  var value = input.value.toString();
-  var char = value.substring(value.length - 1);
-  if (value.length < original.length) {
-    original = original.substring(0, original.length - 1);
-  } else {
-    original = original + char;
-  }
-
-  if (!eye.classList.contains("eye_close")) {
-    var dots = "";
-    for (var i = 0; i < value.length - 1; i++) {
-      dots = dots + dot;
-    }
-    input.value = dots + char;
-    delay(3000).then(() => {
-      value = input.value;
-      if (value.length != 0) {
-        input.value = value.substring(0, value.length - 1) + dot;
-      }
-    });
-  }
-});
-
-function delay(time) {
-  return new Promise((resolve) => setTimeout(resolve, time));
-}
-
-eye.addEventListener("click", () => {
-  var classlist = eye.classList;
-  if (classlist.contains("eye_close")) {
-    classlist.remove("eye_close");
-    var dots = "";
-    for (var i = 0; i < input.value.length - 1; i++) {
-      dots = dots + dot;
-    }
-    input.value = dots;
-  } else {
-    classlist.add("eye_close");
-    input.value = original;
-  }
-});
